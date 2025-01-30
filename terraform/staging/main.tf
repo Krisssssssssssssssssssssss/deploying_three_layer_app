8c8ccc1ea32831bd131e5dbdb5283b8710fc4452
Kristijan Jakimov <kiko.jakimov@gmail.com>
2025-01-30 22:56:18 +0100

    staging added as well

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -9,6 +9,7 @@ module "eks_cluster" {
   cluster_name    = "kris-staging"
   cluster_version = "1.27"
   subnet_ids      = module.network.private_subnet_ids
+  security_group_id    = module.network.eks_security_group_id
   instance_types  = ["t3.medium"]
   desired_size    = 2
   min_size        = 1
