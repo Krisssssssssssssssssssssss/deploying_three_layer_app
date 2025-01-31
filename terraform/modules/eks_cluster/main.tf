d4cc65b90952f45306f4b57a43aae5f007f0a74a
Kristijan Jakimov <kiko.jakimov@gmail.com>
2025-01-31 03:08:25 +0100

    ci-cd pipelines modified

--- terraform/modules/eks_cluster/main.tf
+++ terraform/modules/eks_cluster/main.tf
@@ -19,28 +19,28 @@ resource "aws_eks_cluster" "example" {
 
 
 # # Create a Managed Node Group
-# resource "aws_eks_node_group" "node_group" {
-#   cluster_name    = aws_eks_cluster.example.name
-#   node_group_name = "worker-group"
-#   node_role_arn   = aws_iam_role.node.arn
-#   subnet_ids      = var.subnet_ids
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.example.name
+  node_group_name = "worker-group"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.subnet_ids
 
-#   scaling_config {
-#     desired_size = var.desired_size
-#     min_size     = var.min_size
-#     max_size     = var.max_size
-#   }
+  scaling_config {
+    desired_size = var.desired_size
+    min_size     = var.min_size
+    max_size     = var.max_size
+  }
 
-#   instance_types = var.instance_types
-#   ami_type       = "AL2_x86_64"
+  instance_types = var.instance_types
+  ami_type       = "AL2_x86_64"
 
-#   depends_on = [
-#     aws_eks_cluster.example,
-#     aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
-#     aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
-#     aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy
-#   ]
-# }
+  depends_on = [
+    aws_eks_cluster.example,
+    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy
+  ]
+}
 
 
 # IAM Role for Node Group
